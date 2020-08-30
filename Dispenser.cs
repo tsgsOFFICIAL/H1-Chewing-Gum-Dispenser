2a70add8a4f7d294612661e558b89938215cad3b
Marcus Jensen <marcusproman0507@gmail.com>
2020-08-30 16:10:33 +0200

    Add project files.

---
+++ Dispenser.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace H1_Chewing_Gum_Dispenser
+{
+    public class Dispenser
+    {
+        #region Fields
+        private bool full;
+        private byte fillAmount;
+        private byte amount;
+        private byte blueberryGums;
+        private byte blackberryGums;
+        private byte tuttifruttiGums;
+        private byte orangeGums;
+        private byte strawberryGums;
+        private byte appleGums;
+        private byte blueberryPercent;
+        private byte blackberryPercent;
+        private byte tuttiFruttiPercent;
+        private byte orangePercent;
+        private byte strawberryPercent;
+        private byte applePercent;
+        private string flavor;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// The amount of chewing gum balls left in the machine
+        /// </summary>
+        public byte Amount
+        {
+            get
+            {
+                return amount;
+            }
+        }
+        /// <summary>
+        /// Return false if the machine is empty
+        /// </summary>
+        public bool Full
+        {
+            get
+            {
+                return full;
+            }
+        }
+        #endregion
+
+        #region Constructors
+        public Dispenser(byte amount, byte blueberryPercent, byte blackberryPercent, byte tuttiFruttiPercent, byte orangePercent, byte strawberryPercent, byte applePercent)
+        {
+            full = true;
+            this.fillAmount = amount;
+            this.amount = amount;
+            this.blueberryPercent = blueberryPercent;
+            this.blackberryPercent = blackberryPercent;
+            this.tuttiFruttiPercent = tuttiFruttiPercent;
+            this.orangePercent = orangePercent;
+            this.strawberryPercent = strawberryPercent;
+            this.applePercent = applePercent;
+
+            Refill();
+        }
+        #endregion
+
+        #region Methods
+        public string Draw()
+        {
+            Random random = new Random();
+            flavor = "";
+            while (flavor.Equals("") && amount > 0)
+            {
+                int temp = random.Next(0, 6 + 1);
+                switch (temp)
+                {
+                    case 0:
+                        if (blueberryGums > 0)
+                        {
+                            flavor = "blueberry";
+                            blueberryGums--;
+                        }
+                        break;
+                    case 1:
+                        if (blackberryGums > 0)
+                        {
+                            flavor = "blackberry";
+                            blackberryGums--;
+                        }
+                        break;
+                    case 2:
+                        if (tuttifruttiGums > 0)
+                        {
+                            flavor = "tutti frutti";
+                            tuttifruttiGums--;
+                        }
+                        break;
+                    case 3:
+                        if (orangeGums > 0)
+                        {
+                            flavor = "orange";
+                            orangeGums--;
+                        }
+                        break;
+                    case 4:
+                        if (strawberryGums > 0)
+                        {
+                            flavor = "strawberry";
+                            strawberryGums--;
+                        }
+                        break;
+                    case 5:
+                        if (appleGums > 0)
+                        {
+                            flavor = "apple";
+                            appleGums--;
+                        }
+                        break;
+                }
+            }
+
+            if (amount > 0)
+            {
+                amount--;
+                return $"Here ya go, ma boi, you got a chewing gum with {flavor} flavor!";
+            }
+            else
+            {
+                full = false;
+                return "You've emptied the machine, you little bastard!";
+            }
+        }
+
+        public void Refill()
+        {
+            full = true;
+            amount = fillAmount;
+            blueberryGums = (byte)((double)amount / 100 * (double)blueberryPercent);
+            blackberryGums = (byte)((double)amount / 100 * (double)blackberryPercent);
+            tuttifruttiGums = (byte)((double)amount / 100 * (double)tuttiFruttiPercent);
+            orangeGums = (byte)((double)amount / 100 * (double)orangePercent);
+            strawberryGums = (byte)((double)amount / 100 * (double)strawberryPercent);
+            appleGums = (byte)((double)amount / 100 * (double)applePercent);
+
+            int counter = 0;
+            while (blueberryGums + blackberryGums + tuttifruttiGums + orangeGums + strawberryGums + appleGums < amount)
+            {
+                switch (counter)
+                {
+                    case 0:
+                        blueberryGums++;
+                        break;
+                    case 1:
+                        blackberryGums++;
+                        break;
+                    case 2:
+                        tuttifruttiGums++;
+                        break;
+                    case 3:
+                        orangeGums++;
+                        break;
+                    case 4:
+                        strawberryGums++;
+                        break;
+                    case 5:
+                        appleGums++;
+                        break;
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace H1_Chewing_Gum_Dispenser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dispenser gumDispenser = new Dispenser(55, 25, 12, 20, 19, 14, 10);
+
+            //Main menu
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Ayup lad! Welcome to the machine!");
+                Console.WriteLine("\nDo you want a chewing gum? (Y/N)");
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case 'y':
+                        Console.Clear();
+                        string temp = gumDispenser.Draw();
+                        if (temp.Equals("You've emptied the machine, you little bastard!"))
+                        {
+                            while (!gumDispenser.Full)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("You've emptied the machine, you little bastard!");
+                                Console.WriteLine("Refill the machine? (Y/N)");
+                                switch (Console.ReadKey(true).KeyChar)
+                                {
+                                    case 'y':
+                                        gumDispenser.Refill();
+                                        break;
+                                    case 'n':
+                                        Console.Clear();
+                                        Console.WriteLine("\nOkay, chill weirdo");
+                                        System.Threading.Thread.Sleep(1250);
+                                        Environment.Exit(0);
+                                        break;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine(temp);
+                            System.Threading.Thread.Sleep(400);
+                        }
+                        break;
+                    case 'n':
+                        Console.Clear();
+                        Console.WriteLine("\nOkay, chill weirdo");
+                        System.Threading.Thread.Sleep(1250);
+                        Environment.Exit(0);
+                        break;
+                }
+                Console.Title = $"Gum left: {gumDispenser.Amount}";
+            }
+        }
+    }
+}
